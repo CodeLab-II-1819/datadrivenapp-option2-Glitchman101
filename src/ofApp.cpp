bedaa769a99acf567cb3d68a7fe83012b717d099
Glitchman101 <thomasj.wall@virginmedia.com>
2019-01-21 13:24:08 +0000

    Text Wrapping
    Manage to get the tweets wrapped and formatted the text

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -11,6 +11,17 @@
 //initial app setup
 void ofApp::setup()
 {
+	twitLogo.load("twitterLogo.png");
+
+	btn1.set(150, 100, 50, 50);
+	btn2.set(250, 100, 50, 50);
+	btn3.set(350, 100, 50, 50);
+	btn4.set(150, 200, 50, 50);
+
+
+	myFont.load("font.ttf", 10, true, true); //load font
+	myFont.setLineHeight(20.0f); //set line height of font
+
     //lower app frameRate
     ofSetFrameRate(30);
 
@@ -30,10 +41,10 @@ void ofApp::setup()
     // This starts a simple search for an emoticon.
     client.search(":)");
     // Tweets are retured in the callbacks onStatus(..), onError(...), etc.
-
+	
     /*
-     To design more complex searches, see the the API documentation here:
-     https://dev.twitter.com/rest/public/search
+     To design more complex searches, see thesearch the API documentation here:
+     https://dev.twitter.com/rest/public/
      and:
      https://developer.twitter.com/en/docs/tweets/rules-and-filtering/overview/standard-operators
      You can then use the search object ofxTwitter::SearchQuery like this:
@@ -49,34 +60,45 @@ void ofApp::setup()
      openFrameworks/addons/ofxTwitter/libs/ofxTwitter/include/ofx/Twitter
      In particular search.h
     */
-
 }
 
-/*
- Draw function is used to draw to GUI.
- Current setup simply draws tweet count info to a small GUI
- Tweets are displayed in console
-*/
 void ofApp::draw()
 {
-    //sets background to black
-    ofBackground(0);
+	text = wrapString(tweets.str(), 300);
+
+	ofBackground(192, 222, 237);
+
+	ofSetColor(255);
+	twitLogo.draw(5, 5);
+
+	ofSetColor(255, 0, 255);
+	ofDrawRectangle(btn1);
+
+	ofSetColor(255, 0, 0);
+	ofDrawRectangle(btn2);
+
+	ofSetColor(255, 255, 0);
+	ofDrawRectangle(btn3);
+
+	ofSetColor(0, 255, 0);
+	ofDrawRectangle(btn4);
+
+	
 
     //counts number of tweets
     int total = count + countMissed;
 
+	ofSetColor(0, 0, 0);
+	myFont.drawString(text, 100, 210); //draw text to screen
+
     //string stream used to display number of tweets recived
-    std::stringstream ss;
+    /*std::stringstream ss;
     ss << "  Received: " << count << std::endl;
     ss << "    Missed: " << countMissed << std::endl;
-    ss << "     Total: " << total << std::endl;
-    
-    /*
-     Draw string stream info to the GUI window at x: 14 / y: 14
-     Bitmap string is default text with limited customisation optimisations
-     Load in fonts to enhance design
-    */
-    ofDrawBitmapString(ss.str(), 14, 14);
+    ss << "     Total: " << total << std::endl;*/
+	ofSetColor(0, 0, 0);
+   //ofDrawBitmapString(tweets.str(), 10, 10);
+	
 }
 
 //This function is called everytime the a new tweet is recieved
@@ -86,16 +108,10 @@ void ofApp::onStatus(const ofxTwitter::Status& status)
     count++;
     
     //output the tweet author and text
-    std::cout << "User: " << status.user()->name() << endl;
-    std::cout << "Tweet: " << status.text() << endl;
-    std::cout << "\n -----------------------------\n" << endl;
-    
-    /*
-     To see what other information you can display you should explore
-     the .h files included in:
-     openFrameworks/addons/ofxTwitter/libs/ofxTwitter/include/ofx/Twitter
-     In particular status.h
-    */
+    tweets << "User: " << status.user()->name() << endl;
+    tweets << "Tweet: " << status.text() << endl;
+    tweets << "\n -----------------------------\n" << endl;
+	client.stop();
 }
 
 //returns an error message if error encountered recieving tweets
@@ -113,4 +129,62 @@ void ofApp::onException(const std::exception& notice)
 void ofApp::onMessage(const ofJson& json)
 {
 	// This is the raw message json and is ignored here.
+}
+
+void ofApp::mousePressed(int x, int y, int button) {
+	if (btn1.inside(x, y)) {
+		client.stop();
+		std::cout << "Clicked button 1" << std::endl;
+		tweets.str("");
+		client.search("Code");
+	}
+	if (btn2.inside(x, y)) {
+		client.stop();
+		std::cout << "Clicked button 2" << std::endl;
+		tweets.str("");
+		client.search("#thankful");
+	}
+	if (btn3.inside(x, y)) {
+		client.stop();
+		std::cout << "Clicked button 3" << std::endl;
+		tweets.str("");
+		client.search("Thomas");
+	}
+	if (btn4.inside(x, y)) {
+		client.stop();
+		std::cout << "Clicked button 4" << std::endl;
+		tweets.str("");
+		client.search("BathSpa");
+	}
+
+
+}
+string ofApp::wrapString(string text, int width) {
+	string typeWrapped = "";
+	string tempString = "";
+	vector <string> words = ofSplitString(text, " "); //create vector full of individual words in string passed in
+
+	for (int i = 0; i<words.size(); i++) { //run through vector
+		string wrd = words[i]; //get current word in vector
+
+							   // if we aren't on the first word, add a space
+		if (i > 0) {
+			tempString += " ";
+		}
+		tempString += wrd; //add current word to temp string
+
+		int stringwidth = myFont.stringWidth(tempString); //set string width to length of line
+
+		if (stringwidth >= width) {//check string with to add either space or new line before current word
+			typeWrapped += "\n"; //if line is now longer than desired width add a new line
+			tempString = wrd; // make sure we're including the extra word on the next line
+		}
+		else if (i > 0) {
+			typeWrapped += " "; // if we aren't on the first word, add a space
+		}
+		typeWrapped += wrd; //add current word to string to be returned with new lines for wrapping
+	}
+
+	return typeWrapped;
+
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -8,10 +8,10 @@
 
 #pragma once
 
-
 #include "ofMain.h"
 #include "ofxTwitter.h"
-
+#include <vector>
+#include <sstream>
 
 class ofApp: public ofBaseApp
 {
@@ -19,16 +19,25 @@ public:
     //declare app functions
     void setup();
     void draw();
+	void mousePressed(int x, int y, int button);
     void onStatus(const ofxTwitter::Status& status);
     void onError(const ofxTwitter::Error& error);
     void onException(const std::exception& exception);
 	void onMessage(const ofJson& json);
+
+	ofImage twitLogo;
+	ofRectangle btn1, btn2, btn3, btn4;
     
 	//declare twitter search client
     ofxTwitter::SearchClient client;
     
     //declare variables
+	string wrapString(string text, int width); //string wrapping function
+	ofTrueTypeFont myFont; //font object
+	string text; //string to hold text once wrapped
+	
     int count = 0;
     int countMissed = 0;
+	stringstream tweets;
 
 };
